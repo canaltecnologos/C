a260f8fd562527b82baf8a3327f4e1e5cff4ff9e
canaltecnologos <51940474+canaltecnologos@users.noreply.github.com>
2019-06-19 18:49:30 -0500

    Update 1_2_caracteres_especiales.c

--- Kernighan_excercises/1_2_caracteres_especiales.c
+++ Kernighan_excercises/1_2_caracteres_especiales.c
@@ -2,8 +2,8 @@
 
 int main(){
 	printf("Hola mundo!");
-  printf("\nEl simbolo \\n \n es un salto de linea.\n");
-  printf("\nEl simbolo \\t \t es un espaciado de cuatro caracteres.");
+	printf("\nEl simbolo \\n \n es un salto de linea.\n");
+	printf("\nEl simbolo \\t \t es un espaciado de cuatro caracteres.");
   
 	return 0;
 }
