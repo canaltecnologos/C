aa1418b24ac4ad334de3f766a3c167e56625533f
canaltecnologos <51940474+canaltecnologos@users.noreply.github.com>
2019-06-19 18:49:14 -0500

    1_2_caracteres_especiales.c
    Ejercicio 1-2 del Kernighan completo. Usar "\" para símbolos especiales.

---
+++ Kernighan_excercises/1_2_caracteres_especiales.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+	printf("Hola mundo!");
+  printf("\nEl simbolo \\n \n es un salto de linea.\n");
+  printf("\nEl simbolo \\t \t es un espaciado de cuatro caracteres.");
+  
+	return 0;
+}
