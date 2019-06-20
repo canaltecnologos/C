e24b48c122030d5e409bd7d2bec046f8fb00af86
canaltecnologos <51940474+canaltecnologos@users.noreply.github.com>
2019-06-19 19:49:33 -0500

    1_2_Caracteres_especiales_Vídeo
    Código de vídeo agregado.

--- Ejercicios_Kernighan/1_2_Caracteres_especiales.c
+++ Ejercicios_Kernighan/1_2_Caracteres_especiales.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 
 int main(){
-	printf("Hola mundo!");
-	printf("\nEl simbolo \\n \n es un salto de linea.\n");
-	printf("\nEl simbolo \\t \t es un espaciado de cuatro caracteres.");
-  
+	printf("Hola \n");
+	printf("mundo");
+	printf("\nEl siguiente caracter: \\n produce \nun salto de linea");
+	printf("\nEl siguiente caracter: \\t produce \tun espaciado cuadruple");
+	printf("\nEl siguiente caracter: \\b produce \b un retroceso (borrar un espacio)");
+	printf("\nEl siguiente caracter: \" produce unas comillas \"");
+	
 	return 0;
 }
