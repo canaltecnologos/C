05df66224ebd448447aebe1207570d409a69ec41
canaltecnologos <51940474+canaltecnologos@users.noreply.github.com>
2019-06-22 23:07:20 -0500

    2_Declaracion_variables

---
+++ Tutoriales_Youtube/2_Declaracion_variables.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	int entero1, entero2;
+	float flotante = 10.5;
+	double doble = 10.3;
+	char caracter = 'a';
+	
+	entero1 = 10;
+	entero2 = 20;
+	
+	printf("Primer entero: %d\nSegundo entero: %d\nUn punto flotante: %f\nUn doble: %f\nUn caracter: %c", entero1, entero2, flotante, doble, caracter);
+	
+	return 0;
+}
