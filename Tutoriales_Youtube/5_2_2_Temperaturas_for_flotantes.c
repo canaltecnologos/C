7fd1cc29ee1246b96d37e34cb110f23c09d4cfd9
canaltecnologos <51940474+canaltecnologos@users.noreply.github.com>
2019-06-27 13:03:09 -0500

    Update 5_2_2_Temperaturas_for_flotantes.c

--- Tutoriales_Youtube/5_2_2_Temperaturas_for_flotantes.c
+++ Tutoriales_Youtube/5_2_2_Temperaturas_for_flotantes.c
@@ -7,6 +7,10 @@ int main(){
 	
 	printf("Fahrenheit\tCelsius\n");
 	
+	inicio = 0;
+	final = 300;
+	paso = 20;
+	
 	for (fahr = inicio; fahr <= final; fahr += paso){
 		celsius = 5.0/9.0 * (fahr - 32);
 		printf("%3d\t\t%.3f\n", fahr, celsius);
