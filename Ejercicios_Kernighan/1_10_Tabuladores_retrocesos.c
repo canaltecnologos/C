422ff61ed64c027b446b079727b86e82eb1175c2
Canal tecnólogos <51940474+canaltecnologos@users.noreply.github.com>
2019-07-14 21:59:20 -0500

    Ejercicio 1-10 Kernighan
    * 1_10_Tabuladores_retrocesos
    
    * 1_10_2_Tabuladores_retrocesos_propuesta

---
+++ Ejercicios_Kernighan/1_10_Tabuladores_retrocesos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+main(){
+	int caracter;
+	
+	while((caracter = getchar()) != EOF){
+		if(caracter == '\t'){
+			putchar('\\');
+			putchar('t');
+			//printf("\\t");
+		}
+		else if(caracter == '\b'){
+			putchar('\\');
+			putchar('b');
+			//printf("\\b");
+		}
+		else if (caracter == '\\'){
+			putchar('\\');
+			putchar('\\');
+			//printf("\\\\");
+		}
+		else
+			putchar(caracter);	
+	}
+}
